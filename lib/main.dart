02e46e5bb616740342c73e8d6e39e6033be824ef
Brandon Lackey <44684885+nwohater@users.noreply.github.com>
2023-03-15 20:00:54 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.blue,
+              child: const Text('Container 1'),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.white,
+                child: const Text('Container 2'),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.red,
+                child: const Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
